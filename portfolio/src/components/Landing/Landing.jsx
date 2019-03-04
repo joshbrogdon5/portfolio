5e43d6c51a54ca18a3e397c9f4bacca1a739e8c3
Josh <joshbrogdon5@gmail.com>
2019-03-03 21:04:36 -0700

    landing page backround set

--- portfolio/src/components/Landing/Landing.jsx
+++ portfolio/src/components/Landing/Landing.jsx
@@ -1,8 +1,27 @@
 import React from "react";
+import Sky from "react-sky";
+import "./Landing.css";
 
 export const LandingJSX = () => {
   return (
-    <div>
+    <div class="container">
+      <Sky
+        images={{
+          0: "https://image.flaticon.com/icons/svg/141/141073.svg",
+          1: "https://image.flaticon.com/icons/svg/141/141070.svg",
+          2: "https://image.flaticon.com/icons/svg/141/141009.svg",
+          3: "https://image.flaticon.com/icons/svg/140/140993.svg",
+          4: "https://image.flaticon.com/icons/svg/141/141106.svg",
+          5: "https://image.flaticon.com/icons/svg/141/141015.svg",
+          6: "https://image.flaticon.com/icons/svg/141/141099.svg",
+          7: "https://image.flaticon.com/icons/svg/141/141008.svg",
+          8: "https://image.flaticon.com/icons/svg/141/141036.svg"
+        }}
+        how={60}
+        time={45}
+        size={"100px"}
+        background={"palettedvioletred"}
+      />
       <div class="text">
         Hey, I'm
         <span> Josh Brogdon</span>
