fa2934ea789b1182593c0e68c56f5e63d7416c00
Josh <joshbrogdon5@gmail.com>
2019-04-24 17:30:51 -0600

    everything done with slider except last code excerpt

--- portfolio/src/components/Projects/Projects.js
+++ portfolio/src/components/Projects/Projects.js
@@ -1,43 +1,68 @@
-import React, { Component } from 'react'
-import './Projects.css';
-import Modal from 'react-modal';
+import React, { Component } from "react";
+import "./Projects.css";
+import Modal from "react-modal";
+import Slide from "./Slide";
+import RightArrow from "./Right-Arrow";
+import LeftArrow from "./Left-Arrow";
 
 const customStyles = {
-  content : {
-    top                   : '50%',
-    left                  : '50%',
-    right                 : 'auto',
-    bottom                : 'auto',
-    marginRight           : '-50%',
-    transform             : 'translate(-50%, -50%)'
+  content: {
+    top: "50%",
+    left: "50%",
+    right: "auto",
+    bottom: "auto",
+    marginRight: "-50%",
+    transform: "translate(-50%, -50%)"
   }
 };
 
 export default class Projects extends Component {
-  state = {
-    modalIsOpen: false
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      modalIsOpen: false,
+      images: [
+        "https://s3.us-east-2.amazonaws.com/dzuz14/thumbnails/aurora.jpg",
+        "https://s3.us-east-2.amazonaws.com/dzuz14/thumbnails/canyon.jpg",
+        "https://s3.us-east-2.amazonaws.com/dzuz14/thumbnails/city.jpg",
+        "https://s3.us-east-2.amazonaws.com/dzuz14/thumbnails/desert.jpg",
+        "https://s3.us-east-2.amazonaws.com/dzuz14/thumbnails/mountains.jpg",
+        "https://s3.us-east-2.amazonaws.com/dzuz14/thumbnails/redsky.jpg",
+        "https://s3.us-east-2.amazonaws.com/dzuz14/thumbnails/sandy-shores.jpg",
+        "https://s3.us-east-2.amazonaws.com/dzuz14/thumbnails/tree-of-life.jpg"
+      ],
+      currentIndex: 0
+    };
   }
-  
 
   openModal = () => {
-    this.setState({ modalIsOpen: true })
-  }
+    this.setState({ modalIsOpen: true });
+  };
 
   closeModal = () => {
-    this.setState({ modalIsOpen: false })
-  }
+    this.setState({ modalIsOpen: false });
+  };
+
+  goToNextSlide = () => {
+    this.setState(prevState => ({
+      currentIndex: prevState.currentIndex + 1
+    }));
+  };
+
+  goToPrevSlide = () => {};
 
   render() {
     const { open } = this.state;
     return (
       <div>
-        <div class='projects-container'>
-          <div class='header'>
+        <div class="projects-container">
+          <div class="header">
             <h1>Projects</h1>
-            <div class='header-line'></div>
+            <div class="header-line" />
           </div>
-          <div class='portfolio-container'>
-            <div class='portfolio-piece'>
+          <div class="portfolio-container">
+            <div class="portfolio-piece">
               <img src="" alt="" />
               <button onClick={this.openModal}>Open</button>
               <Modal
@@ -47,15 +72,22 @@ export default class Projects extends Component {
                 style={customStyles}
                 contentLabel="Example Modal"
               >
-
-                <h2 ref={subtitle => this.subtitle = subtitle}>Hello</h2>
+                <h2 ref={subtitle => (this.subtitle = subtitle)}>Hello</h2>
                 <button onClick={this.closeModal}>close</button>
-                <div>I am a modal</div>
+                <div className="slider">
+                
+                  {this.state.images.map((image, i) => (
+                    <Slide key={i} image={image} />
+                  ))}
+
+                  <RightArrow goToNextSlide={this.goToNextSlide} />
+                  <LeftArrow goToPrevSlide={this.goToPrevSlide} />
+                </div>
               </Modal>
             </div>
           </div>
         </div>
       </div>
-    )
+    );
   }
 }
